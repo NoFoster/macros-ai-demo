0088d808c6883f9118397af91b1c938941b3a4da
NoFoster <kfoster0@chicagobooth.edu>
2025-04-29 23:37:06 +0000

    Continuing the form setup

--- app/controllers/macros_controller.rb
+++ app/controllers/macros_controller.rb
@@ -3,4 +3,9 @@ class MacrosController < ApplicationController
   def display_form
   render({ :template => "macro_templates/new_form"})
   end
+
+  def do_magic
+    render({ :template => "macro_templates/results"})
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   # Solutions below. Don't peek until you try it yourself and get stuck!
   get("/blank_form", { :controller => "macros", :action => "display_form"})
 
-  #get("/process_inputs", {:controller =>})
+  post("/process_inputs", { :controller => "macros", :action => "do_magic"})
+  
+  
   get("/solutions/blank_form", { :controller => "solutions", :action => "display_form" })
   
   post("/solutions/process_form", { :controller => "solutions", :action => "process_inputs" })
