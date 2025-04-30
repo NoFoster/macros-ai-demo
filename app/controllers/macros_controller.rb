0a016572ed7e524f5cff2b99bd763cce463c72b8
NoFoster <kfoster0@chicagobooth.edu>
2025-04-30 01:03:05 +0000

    Choices_error_hold

--- app/controllers/macros_controller.rb
+++ app/controllers/macros_controller.rb
@@ -10,11 +10,48 @@ class MacrosController < ApplicationController
 
     @the_image_converted = DataURI.convert(@the_image)
 
-    c = OpenAI::Chat.new
-    c.system("You are an expert nutritionist. Esitmate the macronutrients (carbohydrates, protein, and fat) in grams, as well as total calories in kcal.:")
-    c.user(@the_description)
-    @result = c.assistant!
+    chat = OpenAI::Chat.new
+    chat.model = "GPT-o4 mini"
+    chat.system("You are an expert nutritionist. Esitmate the macronutrients (carbohydrates, protein, and fat) in grams, as well as total calories in kcal.:")
 
+        
+    chat.user(@the_description, image: @the_image)
+
+    chat.schema = '{
+      "name": "nutrition_info",
+      "schema": {
+        "type": "object",
+        "properties": {
+          "carbohydrates": {
+            "type": "integer",
+            "description": "Amount of carbohydrates in grams."
+          },
+          "protein": {
+            "type": "integer",
+            "description": "Amount of protein in grams."
+          },
+          "fat": {
+            "type": "integer",
+            "description": "Amount of fat in grams."
+          },
+          "total_calories": {
+            "type": "integer",
+            "description": "Total calories in kilocalories."
+          }
+        },
+        "required": [
+          "carbohydrates",
+          "protein",
+          "fat",
+          "total_calories"
+        ],
+        "additionalProperties": false
+      },
+      "strict": true
+    }'
+
+    @result = chat.assistant!
+    
 
     render({ :template => "macro_templates/results"})
   end
